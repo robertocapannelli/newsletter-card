f15ecfbf6d07c2c9ba27708e1465d01013ba228e
Roberto Capannelli <roberto.capannelli@gmail.com>
2018-05-18 22:12:00 +0200

    Basic information

--- newsletter-card.php
+++ newsletter-card.php
@@ -1,11 +1,11 @@
 <?php
-
 /*
 Plugin Name: Newsletter Card
-Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
-Description: A brief description of the Plugin.
+Plugin URI: https://walkap.com
+Description: WordPress plugin that works with CF7 and CF7 MailChimp extension to show a fancy subscription form on scrolling page
 Version: 1.0
 Author: robertocapannelli
-Author URI: http://URI_Of_The_Plugin_Author
+Author URI: https://walkap.com
 License: A "Slug" license name e.g. GPL2
+Text Domain: newsletter-card
 */
