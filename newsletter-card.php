e0dbed213b8f34a3e1d60ee15c6d860d10ea4ad3
Roberto Capannelli <roberto.capannelli@gmail.com>
2018-05-30 22:36:20 +0200

    the main plugin file that check if CF7 is installed

--- newsletter-card.php
+++ newsletter-card.php
@@ -1,11 +1,38 @@
 <?php
 /*
 Plugin Name: Newsletter Card
-Plugin URI: https://walkap.com
-Description: WordPress plugin that works with CF7 and CF7 MailChimp extension to show a fancy subscription form on scrolling page
+Plugin URI: https://github.com/robertocapannelli/newsletter-card
+Description: WordPress plugin that works with CF7 plugin to show a fancy subscription form on scrolling page
 Version: 1.0
-Author: robertocapannelli
+Author: Roberto Capannelli
 Author URI: https://walkap.com
-License: A "Slug" license name e.g. GPL2
-Text Domain: newsletter-card
+Text Domain: walkap_cf7nc
 */
+
+/**
+ *
+ * Check if Contect Form 7 exists and is active
+ *
+ */
+function walkap_cf7nc_error() {
+
+	if ( ! file_exists( WP_PLUGIN_DIR . '/contact-form-7/wp-contact-form-7.php' ) ) {
+
+		$walkap_cf7nc_error_out = '<div id="message" class="error is-dismissible"><p>';
+		$walkap_cf7nc_error_out .= __( 'The Contact Form 7 plugin must be installed for the <b>Newsletter card plugin</b> to work. <b><a href="' . admin_url( 'plugin-install.php?tab=plugin-information&plugin=contact-form-7&from=plugins&TB_iframe=true&width=600&height=550' ) . '" class="thickbox" title="Contact Form 7">Install Contact Form 7 Now.</a></b>', 'walkap_cf7nc' );
+		$walkap_cf7nc_error_out .= '</p></div>';
+		echo $walkap_cf7nc_error_out;
+
+	} else if ( ! class_exists( 'WPCF7' ) ) {
+
+		$walkap_cf7nc_error_out = '<div id="message" class="error is-dismissible"><p>';
+		$walkap_cf7nc_error_out .= __( 'The Contact Form 7 is installed, but <strong>you must activate Contact Form 7</strong> below for the <b>Newsletter card plugin</b> to work.', 'walkap_cf7nc' );
+		$walkap_cf7nc_error_out .= '</p></div>';
+		echo $walkap_cf7nc_error_out;
+
+	}
+
+}
+add_action( 'admin_notices', 'walkap_cf7nc_error' );
+
+require_once plugin_dir_path(__FILE__) . '/settings.php';
