bf166ec3a8de4027eff5e480b6895d3c75fa5dc5
Roberto Capannelli <roberto.capannelli@gmail.com>
2018-06-03 18:06:44 +0200

    added front end view php file

---
+++ public/card.php
@@ -0,0 +1,11 @@
+<div class="card-container">
+	<div class="header">
+		<?= get_option('walkap_cf7nc_title') ?>
+	</div>
+	<div class="description">
+		<?= get_option('walkap_cf7nc_description') ?>
+	</div>
+	<div class="content">
+		<?= get_option('walkap_cf7nc_shortcode'); ?>
+	</div>
+</div>
