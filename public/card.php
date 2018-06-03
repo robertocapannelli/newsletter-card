791606e52bc999ad69e7b59f00ba491f58512720
Roberto Capannelli <roberto.capannelli@gmail.com>
2018-06-03 23:30:44 +0200

    newsletter card layout in the front end

--- public/card.php
+++ public/card.php
@@ -1,11 +1,19 @@
-<div class="card-container">
-	<div class="header">
-		<?= get_option('walkap_cf7nc_title') ?>
-	</div>
-	<div class="description">
-		<?= get_option('walkap_cf7nc_description') ?>
-	</div>
-	<div class="content">
-		<?= get_option('walkap_cf7nc_shortcode'); ?>
-	</div>
+<div class="cf7nc-card-container">
+
+    <div class="cf7nc-card-wrapper">
+
+        <div class="header">
+            <h4><?= get_option('walkap_cf7nc_title') ?></h4>
+        </div>
+        <div class="description">
+            <p><?= get_option('walkap_cf7nc_description') ?></p>
+        </div>
+        <div class="content">
+		    <?= do_shortcode(get_option('walkap_cf7nc_shortcode')); ?>
+        </div>
+
+        <span class="close-button">X</span>
+
+    </div>
+
 </div>
