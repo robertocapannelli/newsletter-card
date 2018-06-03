a53e4b6139f25e580f23f4d34d857394253134ce
Roberto Capannelli <roberto.capannelli@gmail.com>
2018-06-03 23:33:09 +0200

    set cookie, reset cookie and show card on scroll, sett a cookie when a close button is pushed

---
+++ public/js/main.js
@@ -0,0 +1,48 @@
+var $ = jQuery.noConflict();
+
+/**
+ * Set a cookie
+ * @param cname - Cookie name
+ * @param cvalue - Cookie value
+ * @param exdays - Expiration days
+ */
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+/**
+ * Reset a previous cookie
+ * @param cname
+ */
+function resetCookie(cname) {
+    document.cookie = cname + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+}
+
+$(document).ready(function () {
+    //resetCookie('is_card_hidden');
+    console.log(document.cookie);
+    //Check if the cookie is set, if not show the newsletter card
+    if (!document.cookie.split(';').filter(function (item) {
+        return item.indexOf('is_card_hidden=true') >= 0
+    }).length) {
+        var scrollPos = 200;
+        //On scroll show the card
+        $(window).scroll(function () {
+            var curScrollPos = jQuery(this).scrollTop();
+            if (curScrollPos > scrollPos) {
+                $('.cf7nc-card-container').addClass('visible');
+            } else {
+                return
+            }
+            scrollPos = curScrollPos;
+        });
+    }
+    //On click close card and set cookie to hide the card in next 7 days
+    $(' .close-button').click(function () {
+        $('.cf7nc-card-container').removeClass('visible');
+        setCookie('is_card_hidden', 'true', 7);
+    });
+});
