3887d95179f3daeaf63c0d5a53b87f2f908f0a50
Roberto Capannelli <roberto.capannelli@gmail.com>
2018-06-03 18:06:07 +0200

    moved some lines to improve readability

--- admin/view.php
+++ admin/view.php
@@ -1,10 +1,4 @@
-<?php
-if ( ! current_user_can( 'manage_options' ) ) {
-	return;
-}
-
-?>
-    <div class="wrap">
+<div class="wrap">
         <h1><?= esc_html( get_admin_page_title() ); ?></h1>
         <form action="options.php" method="post">
 			<?php
