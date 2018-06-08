452770c09bc4ae2ba8729f5fffae5fdba5cc54cf
Roberto Capannelli <roberto.capannelli@gmail.com>
2018-06-08 21:10:12 +0200

    don't show some divs if options are not set

--- public/card.php
+++ public/card.php
@@ -2,12 +2,18 @@
 
     <div class="cf7nc-card-wrapper">
 
-        <div class="header">
-            <h4><?= get_option( 'walkap_cf7nc_title' ) ?></h4>
-        </div>
-        <div class="description">
-            <p><?= get_option( 'walkap_cf7nc_description' ) ?></p>
-        </div>
+		<?php if ( get_option( 'walkap_cf7nc_title' ) ) { ?>
+            <div class="header">
+                <h4><?= get_option( 'walkap_cf7nc_title' ) ?></h4>
+            </div>
+		<?php } ?>
+
+		<?php if ( get_option( 'walkap_cf7nc_description' ) ) { ?>
+            <div class="description">
+                <p><?= get_option( 'walkap_cf7nc_description' ) ?></p>
+            </div>
+		<?php } ?>
+
         <div class="content">
 			<?= do_shortcode( get_option( 'walkap_cf7nc_shortcode' ) ); ?>
         </div>
