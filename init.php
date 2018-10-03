2de308304db95cd69f27b246121d3d96bc3d7ee7
Roberto Capannelli <roberto.capannelli@gmail.com>
2018-10-03 12:55:25 +0200

    Fix - function refactor based on the base name

--- init.php
+++ init.php
@@ -8,7 +8,6 @@ if ( is_admin() ) {
 	//needed to invoke is_plugin_active
 	include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 	$is_active = is_plugin_active( CF7_NC_PLUGIN_DIR . '/newsletter-card.php' );
-
 	//check if a CF7 shortcode is provided and if the plugin is active
 	if ( ! ( $shortcode || $is_active ) || isset( $_COOKIE['is_card_hidden'] ) ) {
 		return;
@@ -16,9 +15,9 @@ if ( is_admin() ) {
 		require_once( CF7_NC_PLUGIN_DIR . 'public/enqueue.php' );
 
 		//Add content to the front-end
-		function walkap_cf7nc_add_content() {
+		function cf7_nc_add_content() {
 			require_once plugin_dir_path( __FILE__ ) . '/public/card.php';
 		}
-		add_action( 'wp_footer', 'walkap_cf7nc_add_content' );
+		add_action( 'wp_footer', 'cf7_nc_add_content' );
 	}
 }
