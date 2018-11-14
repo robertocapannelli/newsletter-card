4a60bd56a5ce245906bd71bc919150befbb221e0
Roberto Capannelli <roberto.capannelli@gmail.com>
2018-11-14 16:12:42 +0100

    Dev - Changed constants due the refactor

--- public/view.php
+++ public/view.php
@@ -1,17 +1,19 @@
-<div class="cf7-nc-card-container">
-    <div class="cf7-nc-card-wrapper">
-		<?php if ( get_option( CF7_NC_PLUGIN_TEXT_DOMAIN . '_title' ) ) { ?>
+<div class="newsletter-card-wrapper">
+    <div class="container">
+		<?php if ( get_option( NEWSLETTER_CARD_TEXT_DOMAIN . '_title' ) ) { ?>
             <div class="header">
-                <h4><?= get_option( CF7_NC_PLUGIN_TEXT_DOMAIN . '_title' ) ?></h4>
+                <h4><?= get_option( NEWSLETTER_CARD_TEXT_DOMAIN . '_title' ) ?></h4>
             </div>
 		<?php } ?>
-		<?php if ( get_option( CF7_NC_PLUGIN_TEXT_DOMAIN . '_description' ) ) { ?>
+		<?php if ( get_option( NEWSLETTER_CARD_TEXT_DOMAIN . '_description' ) ) { ?>
             <div class="description">
-                <p><?= get_option( CF7_NC_PLUGIN_TEXT_DOMAIN . '_description' ) ?></p>
+                <p><?= get_option( NEWSLETTER_CARD_TEXT_DOMAIN . '_description' ) ?></p>
             </div>
 		<?php } ?>
         <div class="content">
-			<?= do_shortcode( get_option( CF7_NC_PLUGIN_TEXT_DOMAIN . '_shortcode' ) ); ?>
+			<?php if ( get_option( NEWSLETTER_CARD_TEXT_DOMAIN . '_shortcode' ) ) { ?>
+				<?= do_shortcode( get_option( NEWSLETTER_CARD_TEXT_DOMAIN . '_shortcode' ) ); ?>
+			<?php } ?>
         </div>
         <span class="close-button">X</span>
     </div>
