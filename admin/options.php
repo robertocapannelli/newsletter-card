f1272f7fde71cc42bace52d0d0df4e3ff73752a0
Roberto Capannelli <roberto.capannelli@gmail.com>
2018-11-14 16:11:51 +0100

    Tweak - Improved admin guide

--- admin/options.php
+++ admin/options.php
@@ -8,7 +8,10 @@ return [
 		'field_id'    => 'shortcode',
 		'field_title' => 'Shortcode *',
 		'is_required' => true,
-		'hint'        => 'Type here your CF7 shortcode, make sure your form has just an email field to have a cooler newsletter card'
+		'hint'        => 'Type here your CF7 shortcode!<br><br><b>Reminder for CF7 configuration (minimum requirements to make things work)</b><br> 1) Make sure you have exactly one email field called your_email like <b>[email* your_email id:email]</b>,
+this field should be mandatory the * symbol does the job.<br>
+		2) All other fields should be wrapped inside a div with class "hidden-fields" like <b>' . htmlentities('<div class="hidden-fields">...</div>') . '</b><br>
+		3) At the end of the content copy and paste this tag <b>' .  htmlentities('<button type="button">Register</button>') . '</b>'
 	],
 	[
 		'option_name' => 'newsletter_card_title',
