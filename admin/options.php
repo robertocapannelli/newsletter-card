ea47dfec0a4c55499acb2ac05a3a500f59c0c415
Roberto Capannelli <roberto.capannelli@gmail.com>
2018-11-27 11:58:22 +0100

    Tewak - Added a complete example in the admin page

--- admin/options.php
+++ admin/options.php
@@ -1,5 +1,34 @@
 <?php
 
+$html = <<<HTML
+
+<div class="field">
+  <label for="email">Email*</label>
+  [email* your_email id:email]
+</div>
+
+<div class="hidden-fields">
+  <div class="field">
+    <label for="name">Name</label>
+    [text your-name id:name placeholder "Your name"]
+  </div>
+  
+  <div class="field">
+    <label for="last-name">Last Name</label>
+    [text last-name id:last-name placeholder "Your last name"]
+  </div>
+
+  <div class="field">
+    <label for="gender">Gender</label>
+    [select gender id:gender include_blank "Male" "Female" "Other"]
+  </div>
+
+</div>
+
+<button type="button" class="submit-button">Register</button>
+
+HTML;
+
 return [
 	[
 		'option_name' => 'newsletter_card_shortcode',
@@ -8,10 +37,13 @@ return [
 		'field_id'    => 'shortcode',
 		'field_title' => 'Shortcode *',
 		'is_required' => true,
-		'hint'        => 'Type here your CF7 shortcode!<br><br><b>Reminder for CF7 configuration (minimum requirements to make things work)</b><br> 1) Make sure you have exactly one email field called your_email like <b>[email* your_email id:email]</b>,
-this field should be mandatory the * symbol does the job.<br>
-		2) All other fields should be wrapped inside a div with class "hidden-fields" like <b>' . htmlentities('<div class="hidden-fields">...</div>') . '</b><br>
-		3) At the end of the content copy and paste this tag <b>' .  htmlentities('<button type="button">Register</button>') . '</b>'
+		'hint'        => 'Type here your CF7 shortcode!<br><br><b>
+							Reminder for CF7 configuration (minimum requirements to make things work)</b><br> 
+							1) Make sure you have exactly one email field called your_email like <b>[email* your_email id:email]</b>,
+							this field should be mandatory the * symbol does the job.<br>
+							2) All other fields should be wrapped inside a div with class "hidden-fields" like <b><code>' . htmlentities('<div class="hidden-fields">...</div>') . '</code></b><br>
+							3) At the end of the content copy and paste this tag <b><code>' .  htmlentities('<button type="button">Register</button>') . '</code></b><br>
+							<h3>Complete Example</h3><code>' . htmlentities($html) .'</code>'
 	],
 	[
 		'option_name' => 'newsletter_card_title',
